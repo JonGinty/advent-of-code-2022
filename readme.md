853b8f26ded3388495578ae513b4c2038adc4cb0
Jon Ginty <j.ginty@outlook.com>
2022-12-06 13:34:35 +0000

    add readme and comment

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Advent of Code 2022
+
+A collection of my submissions for [Advent of Code](https://adventofcode.com/) 2022. Please don't judge me based on code quality here, these are all wacky-hacky quick solutions which I did not refactor after completing the challenges.
--- src/01/index.ts
+++ src/01/index.ts
@@ -2,6 +2,7 @@ import { indexRelative } from "../utils/workingFolder.ts";
 import { buildTotals } from "./buildTotals.ts";
 import { splitByLine } from "./splitByLine.ts";
 
+// I didn't split day 1 into 2 folders, this is my solution for part 2
 const text = await Deno.readTextFile(indexRelative("input.txt"));
 
 const list = splitByLine(text);
