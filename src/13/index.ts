435aa677e3fccfe95be5755d726b7711a6dc4127
Jon Ginty <j.ginty@outlook.com>
2022-12-13 18:23:16 +0000

    shim for day 13

--- src/13/index.ts
+++ src/13/index.ts
@@ -4,12 +4,9 @@ import { indexRelative } from "../utils/workingFolder.ts";
 
 const input = await Deno.readTextFile(indexRelative("input.txt"));
 
-// const path = traverseGrid(new Grid(input), "S", "E");
-const grid = new Grid(input);
-const cost = grid.rankSquares("E", "S");
-//console.log("part 1:", path, path && path.length -1);
-console.log("part 1:", cost);
-console.log("part 2:", grid.lowestRankForInput("E", "a"));
+
+console.log("part 1:", 1);
+console.log("part 2:", 2);
 
 
 
