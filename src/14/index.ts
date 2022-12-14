e49d2c8b70c793ec74c48ba73baca50555c34646
Jon Ginty <j.ginty@outlook.com>
2022-12-14 14:02:05 +0000

    rework part1 solution to allow part 2 to work

--- src/14/index.ts
+++ src/14/index.ts
@@ -1,12 +1,14 @@
 import { indexRelative } from "../utils/workingFolder.ts";
-import { buildGrid, drawGrid, fillWithSand, inputRange, readLines, translate, xy } from "./pathtools.ts";
+import { buildGrid, drawGrid, fillWithSand, inputRange, readLines, xy } from "./pathtools.ts";
 
 const input = await Deno.readTextFile(indexRelative("input.txt"));
 const startPoint: xy = [500,0];
 const g = buildGrid(readLines(input), startPoint);
 drawGrid(g.grid);
 const sand = fillWithSand(g.grid, g.range, startPoint)
-//await Deno.writeTextFile(indexRelative("out.txt"), sand.lastGrid);
+await Deno.writeTextFile(indexRelative("out.txt"), sand.lastGrid);
+
+
 
 console.log("part 1: ", sand.sand);
 console.log("part 2: ", 2);
--- src/14/pathtools.ts
+++ src/14/pathtools.ts
@@ -2,7 +2,7 @@ import { splitByLine } from "../01/splitByLine.ts";
 
 
 export function readLine(line: string): xy[] {
-    return line.split(" -> ").map(s => s.split(",").map(i => parseInt(i)) as xy); 
+    return line.split(" -> ").map(s => s.split(",").map(i => parseInt(i)) as xy);
 }
 
 export function readLines(input: string): xy[][] {
@@ -20,15 +20,15 @@ export function inputRange(input: xy[][], startPoint?: xy): xyRange {
             ins.map(i => i[0]).reduce((p, c) => Math.max(p, c)),
             ins.map(i => i[1]).reduce((p, c) => Math.max(p, c))
         ]
-    } 
+    }
 }
 
 
 export function buildGrid(input: xy[][], startPoint?: xy): {grid: grid, range: xyRange} {
     const range = inputRange(input, startPoint);
     const grid: grid = [];
-    for (let i = 0; i <= yRange(range); i++) {
-        grid[i] = new Array(xRange(range)).fill(".")
+    for (let i = 0; i <= range.max[1]; i++) {
+        grid[i] = new Array(range.max[0] + 1000).fill(".") // ok, I've learned my lesson, no more 2d arrays for grids...
     }
 
     input.forEach(cSet => {
@@ -37,13 +37,13 @@ export function buildGrid(input: xy[][], startPoint?: xy): {grid: grid, range: x
             if (p[0] === c[0]) {
                 //// console.log("match x")
                 for (let i = Math.min(p[1], c[1]); i <= Math.max(p[1], c[1]); i++) {
-                    const [x, y] = translate([p[0], i], range);
+                    const [x, y] = [p[0], i] //translate([p[0], i], range);
                     //// console.log(y,x);
                     grid[y][x] = "#";
                 }
             } else if (p[1] === c[1]) {
                 for (let i = Math.min(p[0], c[0]); i <= Math.max(p[0], c[0]); i++) {
-                    const [x,y] = translate([i, p[1]], range);
+                    const [x,y] = [i, p[1]];
                     //// console.log(y,x);
                     grid[y][x] = "#";
                 }
@@ -57,9 +57,9 @@ export function buildGrid(input: xy[][], startPoint?: xy): {grid: grid, range: x
     return {grid, range};
 }
 
-export function fillWithSand(grid: grid, range: xyRange, startPoint: xy) {
+export function fillWithSand(grid: grid, range: xyRange, startPoint: xy, hitFloor = false) {
     let settled = true;
-    const ts = translate(startPoint, range);
+    const ts = startPoint // translate(startPoint, range);
     // console.log("start", ts)
     let i = 0;
     let lastPath: xy[] | undefined = undefined;
@@ -67,23 +67,23 @@ export function fillWithSand(grid: grid, range: xyRange, startPoint: xy) {
         const path:xy[] = []
         let last = ts;
         settled = false;
-        for (let y = 0; y <= yRange(range); y++) {
+        for (let y = 0; y <= range.max[1]; y++) {
             path.push(last);
             // // console.log(last);
             if (grid[y][last[0]] === ".") {
                 // safe to keep moving
                 // console.log("moving down");
                 last = [last[0],y];
-            } else if (last[0] <= 0) {
+            // } else if (last[0] <= 0) {
 
-                // console.log("off the left");
-                break; // off the left
+            //     // console.log("off the left");
+            //     break; // off the left
             } else if (grid[y][last[0] - 1] === ".") {
                 // console.log("moving down and left");
                 last = [last[0] - 1, y];
-            } else if (last[0] >= xRange(range) -1) {
-                // console.log("off the right");
-                break; // off the right
+            // } else if (last[0] >= xRange(range) -1) {
+            //     // console.log("off the right");
+            //     break; // off the right
             } else if (grid[y][last[0] + 1] === ".") {
                 // console.log("moving down and right");
                 last = [last[0] + 1, y];
@@ -107,11 +107,11 @@ export function fillWithSand(grid: grid, range: xyRange, startPoint: xy) {
     };
 }
 
-export function translate(input: xy, range: xyRange, reverse = false): xy {
-    return reverse ? 
-        [input[0] + range.min[0], input[1] + range.min[1]]: 
-        [input[0] - range.min[0], input[1] - range.min[1]];
-}
+// export function translate(input: xy, range: xyRange, reverse = false): xy {
+//     return reverse ?
+//         [input[0] + range.min[0], input[1] + range.min[1]]:
+//         [input[0] - range.min[0], input[1] - range.min[1]];
+// }
 
 export type xyRange = {
     min: xy;
@@ -127,11 +127,14 @@ export function yRange(range: xyRange) {
 }
 
 export function drawGrid(grid: grid, path?: xy[]) {
+    const range = getGridRange(grid);
+    console.log("range", range);
+
     if (path) {
         let s = "";
-        for (let y = 0; y < grid.length; y++) {
-            for (let x = 0; x < grid[y].length; x++) {
-                // s += grid[y][x] !== "." ? 
+        for (let y = range.min[1]; y <= range.max[1]; y++) {
+            for (let x = range.min[0]; x < range.max[0]; x++) {
+                // s += grid[y][x] !== "." ?
                 //     grid[y][x] :
                 //     path.find(pos => pos[0] === x && pos[1] === y) ?
                 //         "~" :
@@ -148,7 +151,38 @@ export function drawGrid(grid: grid, path?: xy[]) {
         console.log(s);
         return s;
     }
-    
+
+}
+
+function getGridRange(grid: grid): xyRange {
+    const isFull = (item: squareState) => item !== ".";
+
+    let minx: number | undefined = undefined;
+    let miny: number | undefined = undefined;
+    let maxx: number | undefined = undefined;
+    let maxy: number | undefined = undefined;
+
+    for (let y = 0; y < grid.length; y++) {
+        for (let x = 0; x < grid[y].length; x++) {
+            if (isFull(grid[y][x])) {
+                miny = typeof(miny) === "number" ? Math.min(miny, y) : y;
+                minx = typeof(minx) === "number" ? Math.min(minx, x) : x;
+                maxx = typeof(maxx) === "number" ? Math.max(maxx, x) : y;
+                maxy = typeof(maxy) === "number" ? Math.max(maxy, y) : y;
+            }
+        }
+    }
+
+    return {
+        min: [
+            minx ?? 0,
+            miny ?? 0
+        ],
+        max: [
+            maxx ?? 0,
+            maxy ?? 0
+        ]
+    }
 }
 
 export type xy = [number, number];
