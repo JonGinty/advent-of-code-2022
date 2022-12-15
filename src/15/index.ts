40ac8136d25ac6b4302d8865576546c8b1ad9268
Jon Ginty <j.ginty@outlook.com>
2022-12-15 11:32:27 +0000

    day 15 - uncommented edition

--- src/15/index.ts
+++ src/15/index.ts
@@ -5,20 +5,15 @@ import { buildSingleRowRenderer, countEmptySquares, determineFullRange, getAllOf
 const input = await Deno.readTextFile(indexRelative("input.txt"));
 
 const grid = readGridFromInput(input);
-//showScannedSquares(grid);
-// const row = grid.draw({customRange: {...r, minY: 10, maxY: 10}, noOutput: true});
-// const count = [...row].filter(p => p === "#").length;
-// //grid.draw()
+
 
 const distances = readRangesFromInput(input);
 
-// // PART 1 is slow, uncomment for it
-// const fullRange = determineFullRange(distances);
-// const r = { ...fullRange, minY: 2000000, maxY: 2000000 };
-
-// const count = countEmptySquares(grid, r, distances)
-
-// console.log("part 1", count);
+// PART 1 is slow, uncomment for it
+const fullRange = determineFullRange(distances);
+const r = { ...fullRange, minY: 2000000, maxY: 2000000 };
+const count = countEmptySquares(grid, r, distances)
+console.log("part 1", count);
 
 const p2Range: gridRange = {
     minX: 0,
@@ -28,9 +23,7 @@ const p2Range: gridRange = {
 }
 
 const offby1count = getAllOffBy1Squares(distances, p2Range);
-//const offby1count = getAllOffBy1Squares(distances, grid.getRange())
 const n = (offby1count!.x * 4000000) + offby1count!.y
-//const getAllOffByNSquares(distances: )
 
 console.log("part 2", offby1count);
 console.log("part 2", n);
